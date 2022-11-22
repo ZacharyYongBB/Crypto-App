af1f67e06f0da09f071c37d36d68eb12a7d94cb9
ZacharyYongBB <zacharyyong_1997@outlook.com>
2022-11-22 23:21:17 +0800

    added HomeStatsView

--- CryptoSwift/Core/Home/Views/HomeViewModel.swift
+++ CryptoSwift/Core/Home/Views/HomeViewModel.swift
@@ -10,6 +10,13 @@ import Combine
 
 class HomeViewModel: ObservableObject {
     
+    @Published var statistics: [StatisticModel] = [
+        StatisticModel(title: "title", value: "value", percentageChanged: 1),
+        StatisticModel(title: "title", value: "value"),
+        StatisticModel(title: "title", value: "value"),
+        StatisticModel(title: "title", value: "value", percentageChanged: -9)
+    ]
+    
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
     
--- CryptoSwift/Extensions/PreviewProvider.swift
+++ CryptoSwift/Extensions/PreviewProvider.swift
@@ -23,6 +23,10 @@ class DeveloperPreview {
     
     let homeVM = HomeViewModel()
     
+    let stat1 = StatisticModel(title: "Market Cap", value: "$12.9Bn", percentageChanged: 25.34)
+    let stat2 = StatisticModel(title: "Total Volume", value: "$12.3Tr")
+    let stat3 = StatisticModel(title: "Portfolio Value", value: "$21,000,000", percentageChanged: -12.34)
+    
     let coin = CoinModel(
        id: "bitcoin",
        symbol: "btc",
