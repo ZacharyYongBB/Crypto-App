69ce3dfd1e53372828b5872a47cf927500566170
ZacharyYongBB <zacharyyong_1997@outlook.com>
2022-11-30 10:32:27 +0800

    reload list

---
+++ CryptoSwift/Utilities/HapticManager.swift
@@ -0,0 +1,19 @@
+//
+//  HapticManager.swift
+//  CryptoSwift
+//
+//  Created by Zachary on 29/11/22.
+//
+
+import Foundation
+import SwiftUI
+
+class HapticManager {
+    
+    static private let generator = UINotificationFeedbackGenerator()
+    
+    static func notification(type: UINotificationFeedbackGenerator.FeedbackType) {
+        generator.notificationOccurred(type)
+    }
+    
+}
